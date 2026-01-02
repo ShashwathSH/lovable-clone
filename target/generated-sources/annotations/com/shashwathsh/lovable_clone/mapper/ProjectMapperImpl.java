735bd08f1fafc4d0ccc435537d2ac6211bde7450
Azure DevOps Pipeline <pipeline@devops.com>
2026-01-03 01:31:23 +0530

    defined Project APIs

---
+++ target/generated-sources/annotations/com/shashwathsh/lovable_clone/mapper/ProjectMapperImpl.java
@@ -0,0 +1,99 @@
+package com.shashwathsh.lovable_clone.mapper;
+
+import com.shashwathsh.lovable_clone.dto.auth.UserProfileResponse;
+import com.shashwathsh.lovable_clone.dto.project.ProjectResponse;
+import com.shashwathsh.lovable_clone.dto.project.ProjectSummaryResponse;
+import com.shashwathsh.lovable_clone.entity.Project;
+import com.shashwathsh.lovable_clone.entity.User;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-01-03T01:17:30+0530",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.1 (Eclipse Adoptium)"
+)
+@Component
+public class ProjectMapperImpl implements ProjectMapper {
+
+    @Override
+    public ProjectResponse toProjectResponse(Project project) {
+        if ( project == null ) {
+            return null;
+        }
+
+        Long id = null;
+        Instant createdAt = null;
+        Instant updatedAt = null;
+        UserProfileResponse owner = null;
+
+        id = project.getId();
+        createdAt = project.getCreatedAt();
+        updatedAt = project.getUpdatedAt();
+        owner = userToUserProfileResponse( project.getOwner() );
+
+        String email = null;
+
+        ProjectResponse projectResponse = new ProjectResponse( id, email, createdAt, updatedAt, owner );
+
+        return projectResponse;
+    }
+
+    @Override
+    public ProjectSummaryResponse toProjectSummaryResponse(Project project) {
+        if ( project == null ) {
+            return null;
+        }
+
+        Long id = null;
+        Instant createdAt = null;
+        Instant updatedAt = null;
+
+        id = project.getId();
+        createdAt = project.getCreatedAt();
+        updatedAt = project.getUpdatedAt();
+
+        String email = null;
+
+        ProjectSummaryResponse projectSummaryResponse = new ProjectSummaryResponse( id, email, createdAt, updatedAt );
+
+        return projectSummaryResponse;
+    }
+
+    @Override
+    public List<ProjectSummaryResponse> toListOfProjectSummaryResponse(List<Project> projects) {
+        if ( projects == null ) {
+            return null;
+        }
+
+        List<ProjectSummaryResponse> list = new ArrayList<ProjectSummaryResponse>( projects.size() );
+        for ( Project project : projects ) {
+            list.add( toProjectSummaryResponse( project ) );
+        }
+
+        return list;
+    }
+
+    protected UserProfileResponse userToUserProfileResponse(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String email = null;
+        String name = null;
+        String avatarUrl = null;
+
+        id = user.getId();
+        email = user.getEmail();
+        name = user.getName();
+        avatarUrl = user.getAvatarUrl();
+
+        UserProfileResponse userProfileResponse = new UserProfileResponse( id, email, name, avatarUrl );
+
+        return userProfileResponse;
+    }
+}
